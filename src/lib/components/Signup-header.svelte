122286e1db5bc10152f55df8d983e8c2edae50bd
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-12 00:29:31 +0800

    Added signup navbar component

---
+++ src/lib/components/Signup-header.svelte
@@ -0,0 +1,50 @@
+<script>
+	import AxionText from '$lib/components/AxionText.svelte';
+  import Button from '$lib/components/Button.svelte';
+
+  // Menu show or hide boolean for mobile
+  let menuActive = false;
+
+  // Experimental callback for Button
+  const func = () => console.log("Hello world");
+
+  // Subscribe to active in global-stores
+  import { active } from '$lib/stores/global-store';
+</script>
+
+<nav class="navbar is-primary is-spaced">
+  <div class="navbar-brand is-flex is-justify-content-space-between">
+    <div class="navbar-item">
+      <AxionText/>
+    </div>
+  
+    <!-- Navbar burger -->
+    <div class="navbar-item">
+      <div class="navbar-burger {menuActive?"is-active":""}" on:click={()=>menuActive = !menuActive}>
+        <span class="px-4"/>
+        <span class="px-4"/>
+        <span class="px-4"/>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="navbar-menu p-0 {menuActive?"is-active":""}">
+    <div class="navbar-end dm-sans">
+      <a on:click={()=>{active.set("Home")}} href="/" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">Home</a>
+      <a on:click={()=>{active.set("About")}} href="/" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">About</a>
+      <a on:click={()=>{active.set("Contact")}} href="/" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">Contact</a>
+      <a on:click={()=>{active.set("Sign In")}} href="/" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":"undisp"}">Sign In</a>
+    </div>
+  </div>
+</nav>
+
+<style>
+  .navbar-menu {
+    transition: all .2s ease-in;
+  }
+
+  .undisp {
+    display: none;
+  }
+</style>
