ccd0f91b24c0e1f2ee0431ee72f65d5969359ee5
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-11 14:13:16 +0800

    Fixed bug in button where background is just white

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,7 +5,7 @@
   export let text = "Button";
 
   // Type or variant of the button
-  /*
+  /**
   * 
   * primary
   * secondary
@@ -17,10 +17,25 @@
 
   // Additional CSS class
   export let css = "";
+
+  // target link when pressed (optional)
+  export let href = "";
+
+  // Container background override (optional)
+  export let mainBg = "";
 </script>
 
-<MaterialApp>
-  <Button
-    class="button p-5 {css} {type === "primary"?"is-primary":`${type === "secondary"?"has-background-white has-text-grey-dark":`${type === "info"?"is-info":`${type === "link"?"is-link":`${type === "success"?"is-success":`${type === "danger"?"is-danger":`${type==="warning"?"is-warning":""}`}`}`}`}`}`}"
-  >{text}</Button>
-</MaterialApp>
+
+<div class="container">
+  <MaterialApp>
+    <div class="container {mainBg}">
+      <Button
+        class="button p-5 {type === "primary"?"is-primary":`${type === "secondary"?"has-background-white has-text-grey-dark":`${type === "info"?"is-info":`${type === "link"?"is-link":`${type === "success"?"is-success":`${type === "danger"?"is-danger":`${type==="warning"?"is-warning":""}`}`}`}`}`}`} {css}"
+      >
+        <a {href}>
+          {text}
+        </a>
+      </Button>
+    </div>
+  </MaterialApp>
+</div>
