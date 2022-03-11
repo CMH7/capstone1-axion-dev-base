0f6a1f2bc7e8db2b10f40b55b8734d2ac1e4ac1a
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-11 17:38:18 +0800

    Test for global-store.js

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,19 @@
 <script>
   import HomeHeader from "$lib/components/Home-header.svelte";
   import HomeFooter from "$lib/components/Home-footer.svelte";
+
+  import { active } from "$lib/stores/global-store";
+
+  let currentActive;
+  active.subscribe(value => currentActive = value)
 </script>
 
 <HomeHeader />
 
-<div class="section"></div>
+<div class="section {currentActive === "Home"?"has-background-success":""}">
+  <p class="title dm-sans">
+    {currentActive}
+  </p>
+</div>
 
 <HomeFooter/>
