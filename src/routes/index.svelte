6000515bc8daf4d6a7b33bd683f1d6d3021b2d80
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-11 16:56:13 +0800

    Test for footer component with navbar responsively

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import HomeHeader from "$lib/components/Home-header.svelte";
+  import HomeFooter from "$lib/components/Home-footer.svelte";
+</script>
+
+<HomeHeader />
+
+<div class="section"></div>
+
+<HomeFooter/>
