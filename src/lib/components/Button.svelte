ccf08251478556ca58954a0c7ac11ee44c298915
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-11 12:39:16 +0800

    Added base files

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Button, MaterialApp } from 'svelte-materialify';
+
+  // Text inside the button
+  export let text = "Button";
+
+  // Type or variant of the button
+  /*
+  * types
+  * primary
+  * secondary
+  * info
+  * success
+  */
+  export let type = "primary"
+</script>
+
+<MaterialApp>
+  <Button
+    class="button p-5 {type === "primary"?"is-primary":`${type === "secondary"?"has-background-white has-text-grey-dark":`${type === "info"?"is-info":`${type === "link"?"is-link":`${type === "success"?"is-success":`${type === "danger"?"is-danger":`${type==="warning"?"is-warning":""}`}`}`}`}`}`}"
+  >{text}</Button>
+</MaterialApp>
---
+++ src/lib/config/colors.js
@@ -0,0 +1,7 @@
+import colors from 'svelte-materialify/src/utils/colors';
+
+export default {
+  primary: colors.indigo['darken-4'],
+  secondary: "#fff",
+  info: colors.blue['darken-2'],
+}
