1175d91c6eff2513d8c7d09d58def83d7164399d
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-11 16:22:34 +0800

    Change button's containment and text fixed

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -13,29 +13,41 @@
   * success
   * 
   */
-  export let type = "primary"
+  export let type = ""
 
-  // Additional CSS class
+  // Additional CSS classes
   export let css = "";
 
   // target link when pressed (optional)
-  export let href = "";
+  export let href = "/";
 
   // Container background override (optional)
   export let mainBg = "";
+
+  // Rounded button (optional)
+  export let rounded = false;
 </script>
 
 
 <div class="container">
   <MaterialApp>
-    <div class="container {mainBg}">
-      <Button
-        class="button p-5 {type === "primary"?"is-primary":`${type === "secondary"?"has-background-white has-text-grey-dark":`${type === "info"?"is-info":`${type === "link"?"is-link":`${type === "success"?"is-success":`${type === "danger"?"is-danger":`${type==="warning"?"is-warning":""}`}`}`}`}`}`} {css}"
-      >
-        <a {href}>
-          {text}
-        </a>
-      </Button>
+    <div class="block m-0 {mainBg}">
+      <a {href}>
+        <Button
+          {rounded}
+          class="button p-5
+          {
+            type === "primary"?"is-primary":
+            `${type === "secondary"?"has-background-white has-text-grey-dark":
+            `${type === "info"?"is-info":`${type === "link"?"is-link":
+            `${type === "success"?"is-success":`${type === "danger"?"is-danger":
+            `${type==="warning"?"is-warning":"has-text-black"}`}`}`}`}`}`
+          }
+          "
+          >
+          <span class="{css}">{text}</span>
+        </Button>
+      </a>
     </div>
   </MaterialApp>
 </div>
