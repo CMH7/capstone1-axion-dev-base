80df4083e4d87485f9a03706c55e690146822e67
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-11 13:18:36 +0800

    Added Axion Text Logo as component

---
+++ src/lib/components/AxionText.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+  </style>
+</svelte:head>
+
+<h1 class="is-size-1-desktop is-size-2-touch">
+  <a href="/" class="has-text-white">
+    Axion
+  </a>
+</h1>
+
+<style>
+  h1 {
+    font-family: 'Fredoka One', cursive;
+  }
+</style>
