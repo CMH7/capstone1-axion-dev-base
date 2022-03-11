01a5dabab18ec9783f77b65fd2b001fcc004b287
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-11 13:16:00 +0800

    Added "css" attribute in component Button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -6,17 +6,21 @@
 
   // Type or variant of the button
   /*
-  * types
+  * 
   * primary
   * secondary
   * info
   * success
+  * 
   */
   export let type = "primary"
+
+  // Additional CSS class
+  export let css = "";
 </script>
 
 <MaterialApp>
   <Button
-    class="button p-5 {type === "primary"?"is-primary":`${type === "secondary"?"has-background-white has-text-grey-dark":`${type === "info"?"is-info":`${type === "link"?"is-link":`${type === "success"?"is-success":`${type === "danger"?"is-danger":`${type==="warning"?"is-warning":""}`}`}`}`}`}`}"
+    class="button p-5 {css} {type === "primary"?"is-primary":`${type === "secondary"?"has-background-white has-text-grey-dark":`${type === "info"?"is-info":`${type === "link"?"is-link":`${type === "success"?"is-success":`${type === "danger"?"is-danger":`${type==="warning"?"is-warning":""}`}`}`}`}`}`}"
   >{text}</Button>
 </MaterialApp>
