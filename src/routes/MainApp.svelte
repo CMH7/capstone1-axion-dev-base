69f628518026650ba1d4687c93b3855c876cc402
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-13 04:33:32 +0800

    Added MainApp route (Initial)

---
+++ src/routes/MainApp.svelte
@@ -0,0 +1,11 @@
+<script>
+  import MainAppHeader from "$lib/components/MainAppHeader.svelte";
+  import MainAppDrawerSidebar from "$lib/components/MainAppDrawer-sidebar.svelte";
+  import Overlay from "$lib/components/Overlay.svelte";
+  import DashboardInterface from "$lib/interfaces/Dashboard-Interface.svelte";
+</script>
+
+<MainAppHeader/>
+<MainAppDrawerSidebar/>
+<Overlay/>
+<DashboardInterface />
