62e2f181e53c193f8d59cb454840c7cd4dca64b5
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-13 02:21:44 +0800

    Added Overlay when toggling the menus

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { MaterialApp, Overlay } from "svelte-materialify";
+  import { ismini } from "$lib/stores/global-store";
+
+  let active;
+  ismini.subscribe(value => active = !value)
+</script>
+
+<div>
+  <MaterialApp>
+    <Overlay index={1} {active} on:click={()=>ismini.set(true)}/>
+  </MaterialApp>
+</div>
