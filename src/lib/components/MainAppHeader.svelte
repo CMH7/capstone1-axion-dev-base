3d8ddd0592fccbc38d8b1f107569435ecd58ccec
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-13 01:08:59 +0800

    Optimized resources, code and Added more functions

--- src/lib/components/MainAppHeader.svelte
+++ src/lib/components/MainAppHeader.svelte
@@ -1,24 +1,34 @@
 <script>
   import { MaterialApp, AppBar, Button, Icon, Avatar } from "svelte-materialify";
-  import {mdiMenu, mdiAccount, mdiBackburger, mdiForwardburger} from '@mdi/js';
+  import {mdiMenu, mdiAccount, mdiBackburger, mdiForwardburger, mdiWindowClose} from '@mdi/js';
   let collapsed = false;
   let hovered = false;
   let iconHovered = false;
   let burgerHovered = false;
+
+  import { ismini, sidebarActive } from "$lib/stores/global-store";
+  let mini;
+  ismini.subscribe(value=>mini = value);
+  let disabled;
+  sidebarActive.subscribe(value => disabled = !value)
 </script>
 
 <div class="block mb-0">
   <MaterialApp>
-    <AppBar class="px-3 py-1 has-background-primary" {collapsed} >
+    <AppBar class=" py-1 has-background-primary" {collapsed} >
+
+      <!-- Burger -->
       <div slot="icon" on:mouseenter={()=>{burgerHovered = true}} on:mouseleave={()=>{burgerHovered = false}}>
-        <Button class="has-transition {burgerHovered?"has-background-primary":""}" depressed fab text>
-          <Icon class="has-text-white" path={mdiMenu} />
+        <Button {disabled} class="has-transition {burgerHovered?"has-background-primary":""}" depressed fab text>
+          <div on:click={()=>{ismini.set(!mini)}}>
+            <Icon size="30px" class="has-text-white" path={mdiMenu} />
+          </div>
         </Button>
       </div>
 
       <!-- Title -->
       <span class="fredokaone is-size-3" slot="title">
-        <a href="/" class="has-text-white">
+        <a href="/" class="has-text-white is-unselectable">
           AXION
         </a>
       </span>
@@ -28,21 +38,21 @@
 
       <!-- Collapsers -->
       <!-- Collapse the bar -->
-      <div class="is-clickable mr-2 rounded-circle has-transition {hovered?"has-background-grey-lighter":""} {collapsed?"undisp":""}" on:click={()=>{collapsed = true}} on:mouseenter={()=>{hovered = true}} on:mouseleave={()=>{hovered = false}}>
+      <div class="is-clickable mr-2 rounded-circle has-transition {hovered?"has-background-grey-lighter":""} {collapsed?"undisp":""}" on:click={()=>{collapsed = true; sidebarActive.set(false)}} on:mouseenter={()=>{hovered = true}} on:mouseleave={()=>{hovered = false}}>
         <Avatar class="p-5" size="25px">
           <Icon class="has-text-white {hovered?"has-text-primary":""}" path={mdiBackburger} />
         </Avatar>
       </div>
 
       <!-- Uncollapse the bar -->
-      <div class="is-clickable mr-2 rounded-circle has-transition {hovered?"has-background-grey-lighter":""} {collapsed?"":"undisp"}" on:click={()=>{collapsed = false}} on:mouseenter={()=>{hovered = true}} on:mouseleave={()=>{hovered = false}}>
+      <div class="is-clickable mr-2 rounded-circle has-transition {hovered?"has-background-grey-lighter":""} {collapsed?"":"undisp"}" on:click={()=>{collapsed = false; sidebarActive.set(true)}} on:mouseenter={()=>{hovered = true}} on:mouseleave={()=>{hovered = false}}>
         <Avatar class="p-4" size="20px">
           <Icon class="has-text-white {hovered?"has-text-primary":""}" path={mdiForwardburger} />
         </Avatar>
       </div>
 
       <!-- Account Button -->
-      <div class="is-clickable" on:mouseenter={()=>{iconHovered = true}} on:mouseleave={()=>{iconHovered = false}}>
+      <div class="is-clickable {disabled?"undisp":""}" on:mouseenter={()=>{iconHovered = true}} on:mouseleave={()=>{iconHovered = false}}>
         <Avatar class="p-5 has-transition {iconHovered?"has-background-warning":""}" size="35px">
           <Icon class="has-text-white {iconHovered?"has-text-primary":""}" path={mdiAccount}/>
         </Avatar>
