30d7bf30f2d09123751d61f13aebc3faae52f545
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-14 02:15:23 +0800

    Optimized design & code->main cont. of interfaces

--- src/lib/interfaces/Dashboard-Interface.svelte
+++ src/lib/interfaces/Dashboard-Interface.svelte
@@ -1,22 +1,46 @@
 <script>
-  import { currentDashboardSubInterface } from "$lib/stores/global-store";
+	import { subjectColor } from './../stores/global-store.js';
+	import { Icon, MaterialApp } from 'svelte-materialify';
+  import { activeSubject, currentDashboardSubInterface } from "$lib/stores/global-store";
   import SubjectsInterfaces from "./sub-interfaces/Subjects-interfaces.svelte";
+  import WorkspacesInterface from "./sub-interfaces/Workspaces-interface.svelte";
+  import { mdiArrowLeft} from '@mdi/js';
 
   let curDashSubInterface;
   currentDashboardSubInterface.subscribe(value => curDashSubInterface = value);
+
+  let currentActiveSubject;
+  activeSubject.subscribe(value => currentActiveSubject = value);
+
+  let chosenSubjectColor;
+  subjectColor.subscribe(value => chosenSubjectColor = value);
+
+  let ishovering = false;
 </script>
 
 <div class="hero">
   <div class="hero-head px-3">
-    <p class="mb-0 quicksands is-size-1-tablet is-size-2-mobile has-text-weight-bold has-text-info">
+    <p class="mb-0 quicksands is-size-1-tablet is-size-3-mobile has-text-weight-bold has-text-info">
       {#if curDashSubInterface === "Subjects"}
         Subjects
+      {:else if curDashSubInterface === "Workspaces"}
+        <span>
+          <div on:click={()=>{activeSubject.set(""); currentDashboardSubInterface.set("Subjects"); ishovering = false}} class="d-inline-block">
+            <MaterialApp>
+              <div on:mouseenter={()=>ishovering = true} on:mouseleave={()=>ishovering = false} class="is-clickable rounded">
+                <Icon class="{ishovering?"has-text-warning":""}" path={mdiArrowLeft} />
+              </div>
+            </MaterialApp>
+          </div>
+        </span> <span class="has-text-{chosenSubjectColor} is-unselectable">{currentActiveSubject}</span>
       {/if}
     </p>
   </div>
   <div class="hero-body">
     {#if curDashSubInterface === "Subjects"}
       <SubjectsInterfaces />
+    {:else if curDashSubInterface === "Workspaces"}
+      <WorkspacesInterface />
     {/if}
   </div>
 </div>
