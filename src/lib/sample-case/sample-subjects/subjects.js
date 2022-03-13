c25287b3111747e6754651e993cf3b232d791d6f
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-14 00:08:18 +0800

    Removed 'bg' key and value

--- src/lib/sample-case/sample-subjects/subjects.js
+++ src/lib/sample-case/sample-subjects/subjects.js
@@ -1,6 +1,6 @@
 export default {
   subjects: [
-    { name: "Capstone 1", color: "primary", isFavorite: true, bg: 'Axion_Logo2.png'},
+    { name: "Capstone 1", color: "primary", isFavorite: true},
     { name: "Game Development 2", color: "info", isFavorite: false},
     { name: "Information Security", color: "danger", isFavorite: true},
     { name: "Literacy", color: "success", isFavorite: false}
