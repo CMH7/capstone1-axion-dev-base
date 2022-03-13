8fd2ee7f92f2487f9f3ecd353b49c9e95d2adeeb
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-14 00:08:49 +0800

    Added test cases for workspaces

---
+++ src/lib/sample-case/sample-workspaces/workspaces.js
@@ -0,0 +1,8 @@
+export default {
+  workspaces: [
+    { name: "Papers", color: "danger", isFavorite: true},
+    { name: "Research & Development", color: "success", isFavorite: false},
+    { name: "Development", color: "dark", isFavorite: true},
+    { name: "Data Flow Diagram", color: "warning", isFavorite: false}
+  ]
+}
