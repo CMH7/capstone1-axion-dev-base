9731722917b2f7190f84d9f0a8ec09c716aa49c1
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-13 23:38:03 +0800

    Added subjects interface (subInterface->dashboard)

---
+++ src/lib/interfaces/sub-interfaces/Subjects-interfaces.svelte
@@ -0,0 +1,14 @@
+<script>
+import SubjectBox from "$lib/components/interface-components/Subject-box.svelte";
+import subjects from "$lib/sample-case/sample-subjects/subjects";
+
+let allSubjects = subjects.subjects;
+</script>
+
+<div class="columns is-multiline is-variable is-5 pl-6">
+  {#each allSubjects as subject}
+    <div class="column is-narrow">
+      <SubjectBox name={subject.name} color={subject.color} isFavorite={subject.isFavorite}/>
+    </div>
+  {/each}
+</div>
