ac35facc6e3d8fd31e6c398d93dc944f4748ef57
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-14 01:02:36 +0800

    Added Workspaces interface (sub-interface)

---
+++ src/lib/interfaces/sub-interfaces/Workspaces-interface.svelte
@@ -0,0 +1,19 @@
+<script>
+	import WorkspaceBox from '$lib/components/interface-components/sub-interface-components/Workspace-box.svelte';
+  import workspaces from '$lib/sample-case/sample-workspaces/workspaces';
+  import { activeSubject } from '$lib/stores/global-store';
+
+  let allworkspaces = workspaces.workspaces;
+  let currentActiveSubject = "";
+  activeSubject.subscribe(value => currentActiveSubject = value);
+</script>
+
+<div class="columns is-multiline is-variable is-5 pl-6">
+  {#each allworkspaces as workspace}
+    {#if currentActiveSubject === workspace.from}
+      <div class="column is-narrow">
+        <WorkspaceBox name={workspace.name} color={workspace.color} isFavorite={workspace.isFavorite}/>
+      </div>
+    {/if}
+  {/each}
+</div>
