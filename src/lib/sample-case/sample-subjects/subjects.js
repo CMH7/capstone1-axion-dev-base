345c468e31cf2c2a2bdd36fea180e9d60af3547b
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-13 16:09:51 +0800

    Test cases for subject-box usage

---
+++ src/lib/sample-case/sample-subjects/subjects.js
@@ -0,0 +1,8 @@
+export default {
+  subjects: [
+    { name: "Capstone 1", color: "primary", isFavorite: true, bg: 'Axion_Logo2.png'},
+    { name: "Game Development 2", color: "info", isFavorite: false},
+    { name: "Information Security", color: "danger", isFavorite: true},
+    { name: "Literacy", color: "success", isFavorite: false}
+  ]
+}
