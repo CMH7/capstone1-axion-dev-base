bddd0b8c175804f159a09052d8fc57cae6a66888
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-03-20 23:44:04 +0800

    Added functions for anim transition

--- src/lib/components/AxionText.svelte
+++ src/lib/components/AxionText.svelte
@@ -1,5 +1,12 @@
+<script>
+  // stores
+  import { rgb, transitionActive } from "$lib/stores/global-store";
+</script>
+
 <h1 class="is-size-1 fredokaone">
-  <a href="/Home" class="has-text-white">
-    Axion
-  </a>
+  <span on:click={()=> {rgb.set({r: (Math.random() * 256), g: (Math.random() * 256), b: (Math.random() * 256)}); transitionActive.set(true);setTimeout(()=>{transitionActive.set(false)} , 2000)}}>
+    <a href="/Home" class="has-text-white">
+      Axion
+    </a>
+  </span>
 </h1>
