140ae6d127baf1bbd9999166e1e2cd1240d707ee
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-04-03 15:02:09 +0800

    Added Loading screens

---
+++ src/lib/components/LoadingScreen.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { fade, slide } from 'svelte/transition';
+  import { Pulse } from 'svelte-loading-spinners';
+</script>
+
+<div out:fade class="hero is-fullheight is-primary">
+  <div class="hero-body is-flex is-justify-content-center is-align-items-center">
+    <Pulse
+      size=130
+      color="#fff"
+    />
+  </div>
+</div>
+
+<style>
+  .hero {
+    z-index: 99;
+    position: absolute;
+    width: 100vw;
+  }
+</style>
