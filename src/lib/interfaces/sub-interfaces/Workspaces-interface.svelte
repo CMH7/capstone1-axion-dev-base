bf1a1ea305eb8869a44cb46bfd03e321f1cd5a9a
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-04-10 19:45:19 +0800

    Refactor nothing to render in workspace interface

--- src/lib/interfaces/sub-interfaces/Workspaces-interface.svelte
+++ src/lib/interfaces/sub-interfaces/Workspaces-interface.svelte
@@ -9,14 +9,33 @@
   let allworkspaces = workspaces.workspaces;
   let currentActiveSubject = "";
   activeSubject.subscribe(value => currentActiveSubject = value);
+
+  // show if theres no matching from workspace to current subject
+  let show = true;
+  let showproof = 0;
+  allworkspaces.forEach(workspace => {
+    if(workspace.from === currentActiveSubject) showproof++;
+  });
+
+  if(showproof > 0) show = false;
 </script>
 
 <div class="columns is-multiline is-variable is-5 pl-6">
-  {#each allworkspaces as workspace}
-    {#if currentActiveSubject === workspace.from}
-      <div in:fade class="column is-narrow">
-        <WorkspaceBox name={workspace.name} color={workspace.color} isFavorite={workspace.isFavorite}/>
+  {#if show}
+    <div class="section">
+      <div class="container">
+        <p>
+          No workspaces
+        </p>
       </div>
-    {/if}
-  {/each}
+    </div>
+  {:else}
+    {#each allworkspaces as workspace}
+      {#if currentActiveSubject === workspace.from}
+        <div in:fade class="column is-narrow">
+          <WorkspaceBox name={workspace.name} color={workspace.color} isFavorite={workspace.isFavorite}/>
+        </div>
+      {/if}
+    {/each}
+  {/if}
 </div>
