12a08379eb3a26166c938967f9cddce6153501a6
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-05-02 11:41:44 +0800

    Disables the back functionality onload of MainApp

--- src/routes/MainApp.svelte
+++ src/routes/MainApp.svelte
@@ -1,10 +1,22 @@
 <script>
+  // When user logs in then this script will trigger
+  // This script will disable the back button when inside the main app
+  import {onMount} from 'svelte';
+  onMount(()=>{
+    history.pushState(null, null, location.href);
+    window.onpopstate = function () {
+      history.go(1);
+    };
+  });
+  // end of the disabling back button script
+
+  // Imports
   import { fade } from 'svelte/transition';
   import MainAppHeader from "$lib/components/MainAppHeader.svelte";
   import MainAppDrawerSidebar from "$lib/components/MainAppDrawer-sidebar.svelte";
   import Overlay from "$lib/components/Overlay.svelte";
   import DashboardInterface from "$lib/interfaces/Dashboard-Interface.svelte";
-  import { currentIndex, currentInterface, ismini, sidebarActive } from "$lib/stores/global-store";
+  import { currentInterface, ismini, sidebarActive } from "$lib/stores/global-store";
   import AssignedToMeInterface from "$lib/interfaces/Assigned-to-me-interface.svelte";
   import FavoritesInterface from "$lib/interfaces/Favorites-interface.svelte";
   import CalendarInterface from "$lib/interfaces/Calendar-interface.svelte";
