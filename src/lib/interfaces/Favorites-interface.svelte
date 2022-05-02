85c90c5534d7b310a8b0ae4af103daffc3d1685a
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-05-02 13:53:04 +0800

    Added create new subj button

--- src/lib/interfaces/Favorites-interface.svelte
+++ src/lib/interfaces/Favorites-interface.svelte
@@ -4,9 +4,12 @@
 </script>
 
 <div in:fade class="hero">
-  <div class="hero-body">
-    <p>
+  <div class="hero-head px-3">
+    <p class="mb-0 quicksands is-size-1-tablet is-size-3-mobile has-text-weight-bold has-text-yellow is-unselectable">
       Favorites
     </p>
+  </div>
+  <div class="hero-body">
+    
   </div>
 </div>
--- src/lib/interfaces/sub-interfaces/Subjects-interfaces.svelte
+++ src/lib/interfaces/sub-interfaces/Subjects-interfaces.svelte
@@ -1,17 +1,69 @@
 <script>
   import SubjectBox from "$lib/components/interface-components/Subject-box.svelte";
   import subjects from "$lib/sample-case/sample-subjects/subjects";
+  import { mdiPlus } from "@mdi/js";
+  import { MaterialApp, Icon, Tooltip } from "svelte-materialify";
 
   // Transition
   import { fade } from 'svelte/transition';
  
   let allSubjects = subjects.subjects;
+  
+  // mouse hover effect
+  let hovering = false;
+
+  //hint
+  import { useHint } from "$lib/stores/global-store";
+  import AddSubjectPopUp from "$lib/components/interface-components/sub-interface-components/Add-subject-pop-up.svelte";
+  let hintAvailable;
+  useHint.subscribe(value => hintAvailable = value);
 </script>
 
 <div class="columns is-multiline is-variable is-5 pl-6">
+  <!-- Add button -->
+
+  {#if hintAvailable}
+  <Tooltip bottom>
+    <div on:mouseenter={() => hovering = true} on:mouseleave={() => hovering = false} in:fade on:click={() => AddSubjectPopUp} class="column is-narrow">
+      <div class="has-transition notification rounded-xl has-background-grey-{hovering ? "dark" : "light"} is-clickable is-flex is-justify-content-center is-align-items-center px-6">
+        <div>
+          <MaterialApp>
+            <div class="has-transition is-clickable has-background-grey-{hovering ? "dark" : "light"}">
+              <Icon size="40px" class="{hovering ? "grey-text lighten-3" : ""}" path={mdiPlus}/>
+            </div>
+          </MaterialApp>
+        </div>
+      </div>
+    </div>
+    <span slot = "tip">
+      Create new subject
+    </span>
+  </Tooltip>
+  {:else}
+  <div on:mouseenter={() => hovering = true} on:mouseleave={() => hovering = false} in:fade class="column is-narrow">
+    <div class="has-transition notification rounded-xl has-background-grey-{hovering ? "dark" : "light"} is-clickable is-flex is-justify-content-center is-align-items-center px-6">
+      <div>
+        <MaterialApp>
+          <div class="has-transition is-clickable has-background-grey-{hovering ? "dark" : "light"}">
+            <Icon size="40px" class="{hovering ? "grey-text lighten-3" : ""}" path={mdiPlus}/>
+          </div>
+        </MaterialApp>
+      </div>
+    </div>
+  </div>
+  {/if}
+
+  <!-- Other Subjects -->
   {#each allSubjects as subject}
     <div in:fade class="column is-narrow">
       <SubjectBox name={subject.name} color={subject.color} isFavorite={subject.isFavorite} />
     </div>
   {/each}
 </div>
+
+<style>
+  .notification {
+    width: 250px;
+    height: 110px;
+  }
+</style>
