bc5649b042de2129ee0d4b3b64d98aa7e9419801
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-05-02 17:47:39 +0800

    Added workspace members

--- src/lib/sample-case/sample-workspaces/workspaces.js
+++ src/lib/sample-case/sample-workspaces/workspaces.js
@@ -1,8 +1,30 @@
+import members from "../sample-members/members";
+
 export default {
   workspaces: [
-    {from: "Capstone 1", name: "Papers", color: "danger", isFavorite: true},
-    {from: "Capstone 1", name: "Research & Development", color: "success", isFavorite: false},
-    {from: "Capstone 1", name: "Development", color: "primary", isFavorite: true},
-    {from: "Literacy", name: "Data Flow Diagram", color: "warning", isFavorite: false}
+    {from: "Capstone 1", name: "Papers", color: "danger", isFavorite: true, members: [
+      members.members[0],
+      members.members[1],
+      members.members[2],
+      members.members[3],
+    ]},
+    {from: "Capstone 1", name: "Research & Development", color: "success", isFavorite: false, members: [
+      members.members[2],
+      members.members[1],
+      members.members[3],
+      members.members[4],
+    ]},
+    {from: "Capstone 1", name: "Development", color: "primary", isFavorite: true, members: [
+      members.members[5],
+      members.members[1],
+      members.members[0],
+      members.members[3],
+    ]},
+    {from: "Literacy", name: "Data Flow Diagram", color: "warning", isFavorite: false, members: [
+      members.members[2],
+      members.members[5],
+      members.members[1],
+      members.members[3],
+    ]}
   ]
 }
