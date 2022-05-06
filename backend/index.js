02a7f5e5029685ce2360a60acd1436f38e77d695
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-05-06 16:52:42 +0800

    Added API routes

--- backend/index.js
+++ backend/index.js
@@ -29,32 +29,111 @@ const port = 8080;
 // === SERVER ===
 
 
-// Get route
-app.get('/', async (req, res) => {
+// ====== CREATE ======
+// ###### POST ROUTES ######
+
+// Create new user
+app.post('/Signup', async (req, res) => {
   conn();
 
-  console.log(req.body);
+  const newUser = await prisma.users.create({
+    data: req.body
+  });
 
-  if (Object.entries(req.body).length === 0) {
-    const allUsers = await prisma.users.findMany();
-    res.send(allUsers);
-    console.log('null');
-  } else {
-    const user = await prisma.users.findFirst({
-      select: {
-        subjects: true
-      },
-      where: {
-        email: {
-          equals: `${req.body.email}`
-        }
-      }
-    });
-    res.send(user);
-    console.log('Not null');
-  }
+  res.send({
+    status: res.statusCode,
+    valid: true
+  });
+
   disconn();
 });
+
+// Creates a new subject
+app.post('/MainApp', async (req, res) => {
+  conn();
+
+  const user = await prisma.users.findFirst({
+    where: {
+      id: {
+        equals: req.query.id
+      }
+    }
+  });
+
+  const subjectCounts = user.subjects.length;
+
+  const usera = await prisma.users.update({
+    where: {
+      id: req.query.id
+    },
+    data: {
+      subjects: {
+        push: {
+          color: req.query.color,
+          id: subjectCounts + 1,
+          isFavorite: false,
+          name: req.query.name,
+          workspaces: []
+        }
+      }
+    }
+  });
+
+  res.send(usera);
+  disconn();
+});
+
+
+
+// ====== READ ======
+// Get route
+
+// Gets all user from database
+app.get('/', async (req, res) => {
+  conn();
+  const allUsers = await prisma.users.findMany();
+  res.send(allUsers);
+  disconn();
+});
+
+// Gets only password to sign in
+app.get('/Signin', async (req, res) => {
+  conn();
+  const user = await prisma.users.findFirst({
+    select: {
+      password: true
+    },
+    where: {
+      email: {
+        equals: `${req.query.email}`
+      }
+    }
+  });
+  res.send({
+    status: res.statusCode,
+    data: user
+  });
+  disconn();
+});
+
+// Gets the user based on the email
+app.post('/validUser', async (req, res) => {
+  conn();
+  const user = await prisma.users.findFirst({
+    where: {
+      email: {
+        equals: req.body.email
+      }
+    }
+  });
+
+  res.send(user);
+  disconn();
+});
+
+// ====== UPDATE ======
+
+// ====== DELETE ======
 
 app.listen(port, function () {
   console.log("Started application backend on port %d", port);
