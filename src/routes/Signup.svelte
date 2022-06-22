11a48c82a6d6fedb077883c3df43db13b5f76843
Joannerazelle <Joannerazelle19@gmail.com>
2022-06-22 14:50:20 +0800

    T-227-Fix/Sign-up-field-'gender'

--- src/routes/Signup.svelte
+++ src/routes/Signup.svelte
@@ -1,7 +1,7 @@
 <script>
   import HomeFooter from "$lib/components/Home-footer.svelte";
   import SignupHeader from "$lib/components/Signup-header.svelte";
-  import { Icon, Divider, MaterialApp } from "svelte-materialify";
+  import { Icon, Divider, Select, MaterialApp } from "svelte-materialify";
   import {mdiGoogle, mdiFacebook} from "@mdi/js";
   import {fade} from 'svelte/transition';
   import axios from 'axios';
@@ -9,6 +9,12 @@
   import {notifs} from '$lib/stores/global-store';
   import NotificationContainer from "$lib/components/Notification-container.svelte";
   import Notification from "$lib/components/Notification.svelte";
+
+
+  const items = [
+    {name: 'Male', value: 'Male'},
+    {name: 'Female', value: 'Female'}
+  ]
 
     // inputs values
   let firstName = "",
@@ -121,102 +127,109 @@
   {/each}
 </NotificationContainer>
 
-<SignupHeader/>
-<div in:fade class="hero is-fullheight-with-navbar">
-  <div class="hero-head">
-    <p class="is-size-1-tablet is-size-2-mobile fredokaone has-text-black has-text-weight-bold has-text-centered my-6">SIGN UP</p>               
-  </div>
-
-  <div class="hero-body p-0">
-    <div class="columns is-mobile is-centered is-multiline">
-      <!-- left side input -->
-      <div class="column is-4-tablet is-8-mobile">
-        <div class=" d-flex flex-wrap">
-
-          <!-- First name -->
-          <input required bind:value={firstName} class="input quicksands has-background-light" style="width: 100%;" type="text" placeholder="First Name">
-          
-          <!-- Last name -->
-          <input required bind:value={lastName} class="input quicksands mt-3 has-background-light" style="width: 100%;" type="text" placeholder="Last Name">
-
-          <!-- Age -->
-          <input required bind:value={age} class="input quicksands my-3 has-background-light" style="width: 30%; margin-right: 5%" type="text" placeholder="Age">
-
-          <!-- Gender -->
-          <input required bind:value={gender} class="input quicksands my-3 has-background-light" style="width: 65%;" type="text" placeholder="Gender">
-          
-          <!-- E-mail -->
-          <input required bind:value={email} class="input quicksands has-background-light" style="width: 100%;" type="text" placeholder="Email">
-
-        </div>
-      </div>
-
-      <!-- right side input -->
-      <div class="column is-4-tablet is-8-mobile">
-        <div class=" d-flex flex-wrap">
-
-          <!-- School name -->
-          <input required bind:value={school} class="input quicksands has-background-light" style="width: 100%;" type="text" placeholder="School/University">
-
-          <!-- Course -->
-          <input required bind:value={course} class="input quicksands my-3 has-background-light" style="width: 100%;" type="text" placeholder="Course">
-          
-          <!-- Year -->
-          <input required bind:value={year} class="input quicksands mb-3 has-background-light" style="width: 100%;" type="text" placeholder="Year">
-
-          <!-- Password -->
-          <input required bind:value={password} class="input quicksands has-background-light" style="width: 47%; margin-right: 5%" type="password" placeholder="Password">
-          
-          <!-- Re-password -->
-          <input required bind:value={repassword} class="input quicksands has-background-light" style="width: 48%;" type="password" placeholder="Confirm Password">
-
-        </div>
-      </div>
-      
-      <div class="column p-0 is-12"/>
-      <div class="column p-0 is-5">
-        <MaterialApp>
-          <Divider />
-        </MaterialApp>
-        </div>
-      <div class="column p-0 is-12"/>
-
-      <div class="column p-0 is-12">
-        <p class="m-0 is-size-5 has-text-black has-text-weight-semibold has-text-centered">with</p>
-      </div>
-
-      <div class="column p-0 is-12 py-5">
-        <div class="is-flex is-justify-content-center">
-          <div class="mx-3">
-            <MaterialApp>
-              <a href="http://gmail.com/" target="_blank">
-                <Icon class="has-text-danger-dark" size="40px" path={mdiGoogle} />
-              </a>
-            </MaterialApp>
-          </div>
-
-          <div class="mx-3">
-            <MaterialApp>
-              <a href="http://facebook.com/" target="_blank">
-                <Icon class="has-text-info" size="40px" path={mdiFacebook} />
-              </a>
-            </MaterialApp>
-          </div>
-        </div>
-      </div>
-
-      <div class="column is-12 p-0 pt-7 mb-5">
-        <div class="is-flex flex-column is-align-items-center">
-          <button on:click={createNewUser} class="button is-small rounded-xl is-primary dm-sans has-text-weight-bold is-size-4">Sign Up</button>
-          <p class="pt-4 is-size-6 dm-sans">Already have an account? Click <a href="/Signin">Sign in</a></p>
-        </div>
-      </div>
-
+<MaterialApp>
+  <SignupHeader/>
+  <div in:fade class="hero is-fullheight-with-navbar">
+    <div class="hero-head">
+      <p class="is-size-1-tablet is-size-2-mobile fredokaone has-text-black has-text-weight-bold has-text-centered my-6">SIGN UP</p>               
     </div>
-  </div>
   
-  <div class="hero-foot">
-    <HomeFooter/>
-  </div>
+    <div class="hero-body p-0">
+      <div class="columns is-mobile is-centered is-multiline">
+        <!-- left side input -->
+        <div class="column is-4-tablet is-8-mobile">
+          <div class=" d-flex flex-wrap">
+  
+            <!-- First name -->
+            <input required bind:value={firstName} class="input quicksands has-background-light" style="width: 100%;" type="text" placeholder="First Name">
+            
+            <!-- Last name -->
+            <input required bind:value={lastName} class="input quicksands mt-3 has-background-light" style="width: 100%;" type="text" placeholder="Last Name">
+  
+            <!-- Age -->
+            <input required bind:value={age} class="input quicksands my-3 has-background-light" style="width: 65%; margin-right: 5%" type="text" placeholder="Age">
+  
+            <!-- Gender -->
+            <div class="select quicksands my-3" style="width: 30%;">
+              <select class="has-background-light" style="padding-right: 60%;" >
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </div>
 
-</div>
+            <!-- E-mail -->
+            <input required bind:value={email} class="input quicksands has-background-light" style="width: 100%;" type="text" placeholder="Email">
+  
+          </div>
+        </div>
+  
+        <!-- right side input -->
+        <div class="column is-4-tablet is-8-mobile">
+          <div class=" d-flex flex-wrap">
+  
+            <!-- School name -->
+            <input required bind:value={school} class="input quicksands has-background-light" style="width: 100%;" type="text" placeholder="School/University">
+  
+            <!-- Course -->
+            <input required bind:value={course} class="input quicksands my-3 has-background-light" style="width: 100%;" type="text" placeholder="Course">
+            
+            <!-- Year -->
+            <input required bind:value={year} class="input quicksands mb-3 has-background-light" style="width: 100%;" type="text" placeholder="Year">
+  
+            <!-- Password -->
+            <input required bind:value={password} class="input quicksands has-background-light" style="width: 47%; margin-right: 5%" type="password" placeholder="Password">
+            
+            <!-- Re-password -->
+            <input required bind:value={repassword} class="input quicksands has-background-light" style="width: 48%;" type="password" placeholder="Confirm Password">
+  
+          </div>
+        </div>
+        
+        <div class="column p-0 is-12"/>
+        <div class="column p-0 is-5">
+          <MaterialApp>
+            <Divider />
+          </MaterialApp>
+          </div>
+        <div class="column p-0 is-12"/>
+  
+        <div class="column p-0 is-12">
+          <p class="m-0 is-size-5 has-text-black has-text-weight-semibold has-text-centered">with</p>
+        </div>
+  
+        <div class="column p-0 is-12 py-5">
+          <div class="is-flex is-justify-content-center">
+            <div class="mx-3">
+              <MaterialApp>
+                <a href="http://gmail.com/" target="_blank">
+                  <Icon class="has-text-danger-dark" size="40px" path={mdiGoogle} />
+                </a>
+              </MaterialApp>
+            </div>
+  
+            <div class="mx-3">
+              <MaterialApp>
+                <a href="http://facebook.com/" target="_blank">
+                  <Icon class="has-text-info" size="40px" path={mdiFacebook} />
+                </a>
+              </MaterialApp>
+            </div>
+          </div>
+        </div>
+  
+        <div class="column is-12 p-0 pt-7 mb-5">
+          <div class="is-flex flex-column is-align-items-center">
+            <button on:click={createNewUser} class="button is-small rounded-xl is-primary dm-sans has-text-weight-bold is-size-4">Sign Up</button>
+            <p class="pt-4 is-size-6 dm-sans">Already have an account? Click <a href="/Signin">Sign in</a></p>
+          </div>
+        </div>
+  
+      </div>
+    </div>
+    
+    <div class="hero-foot">
+      <HomeFooter/>
+    </div>
+  
+  </div>
+</MaterialApp>
