4a57d1d80b213fa3e7294529f10a04a60fa989be
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-06-28 22:10:31 +0800

    created the coming soon modal component

---
+++ src/lib/components/ComingSoonModal.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { Dialog, MaterialApp, Avatar } from 'svelte-materialify';
+  
+    export let active = false;
+</script>
+  
+<MaterialApp>
+  <Dialog class="pa-4 has-background-primary max-w-400" bind:active>
+    <div class="is-flex is-align-items-center">
+        <div class="is-flex-shrink-0">
+            <Avatar>
+                <img src="Axion_Logo.png" alt="Avatar" />
+            </Avatar>
+        </div> 
+        <div class="is-flex-grow-1">
+            <div class="is-flex is-flex-direction-column is-align-items-center">
+                <div class="fredokaone has-text-white is-size-3 is-size-5-mobile">
+                    COMING SOON!
+                </div>
+                <div class="dmsans has-text-white is-size-7">
+                    This feature is under development.
+                </div>
+            </div>
+        </div> 
+    </div>
+  </Dialog>
+</MaterialApp>
