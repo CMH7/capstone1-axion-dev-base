9fc55a9e4a259c427577c9b34bb4bb974acdf693
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-07-19 23:36:02 +0800

    Moved the system notification files

---
+++ src/lib/components/System-Notification/Notification-container.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { notifs } from '$lib/stores/global-store'
+  import Notification from './Notification.svelte'
+</script>
+
+<div style="position: fixed; top: 2%; right: 1%; z-index: 50" class="{$notifs.length > 0? "": "undisp" } maxmins-w-400-dt-to-mb-90p max-h-100p">
+  {#each $notifs as notif}
+    <Notification msg="{notif.msg}" type="{notif.type}" id={notif.id} />
+  {/each}
+</div>
---
+++ src/lib/components/System-Notification/Notification.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { notifs } from '$lib/stores/global-store'
+  import { fade } from 'svelte/transition'
+
+  export let msg = ""
+  export let type = "error"
+  export let id
+
+  let notifsCopy = $notifs
+
+  let closed = false
+
+</script>
+
+<div in:fade out:fade class="notification is-flex is-justify-content-space-between is-align-items-center mb-2 is-{type === "success"? "success": "danger"} is-light {closed ? "undisp": ""}">
+  {msg}
+  <div
+    class="delete"
+    on:click={
+      e => {
+        notifsCopy = notifsCopy.filter(val => val.id != id)
+        notifs.set(notifsCopy)
+        closed = true
+      }
+    }
+  />
+</div>
