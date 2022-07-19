2f6023367c3774f56886f1c43acb3b55d08ce07c
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-07-19 22:53:21 +0800

    Changed Contact link

--- src/lib/components/Signup-header.svelte
+++ src/lib/components/Signup-header.svelte
@@ -28,11 +28,11 @@
 
   <div class="navbar-menu p-0 {menuActive?"is-active":""}">
     <div class="navbar-end dm-sans">
-      <a on:click={()=>{active.set("Home"); transitionActive.set(true);setTimeout(()=>{transitionActive.set(false)} , 2000)}} href="/Home" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">Home</a>
+      <a on:click={()=>{active.set("Home"); transitionActive.set(true);setTimeout(()=>{transitionActive.set(false)} , 2000)}} href="/" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">Home</a>
       <a on:click={()=>{active.set("About")}} href="/" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">About</a>
       
         <!-- WORK OF JOANNE CONTACT FORM PAGE -->
-      <a on:click={()=>{active.set("Contact")}} href="/Contactform" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">Contact</a>
+      <a on:click={()=>{active.set("Contact")}} href="/Contact" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":""}">Contact</a>
       <a on:click={()=>{active.set("Sign In")}} href="/Signin" class="navbar-item is-size-5 {menuActive?"py-4 has-text-centered":"undisp"}">Sign In</a>
       <div on:click={()=>{active.set("Sign In")}} class="navbar-item {menuActive?"undisp":""}">
         <Button text="Sign In" href="/Signin" textcss="is-size-5-desktop has-text-weight-bold" mainBg="has-background-primary"/>
