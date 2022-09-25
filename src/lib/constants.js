e93d8c1872378a361f289a89801af8050439e7bf
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-09-25 13:51:38 +0800

    Changed the backURI to axion-back

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -81,7 +81,7 @@ export default {
     status: "",
     level: 1  
   },
-  backURI: "https://axion-backend.herokuapp.com",
+  backURI: "https://axion-back.herokuapp.com",
   lowerCasedLetters: new String('abcdefghijklmnopqrstuvwxyz').split(''),
   upperCasedLetters: new String('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split(''),
   digits: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
