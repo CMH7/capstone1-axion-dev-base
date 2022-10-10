3a77a89cc04a7f4d51171cda8be552e6c0cda940
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-10 14:48:10 +0800

    Added Favorites icon and refactored code

--- src/lib/components/interface-components/sub-interface-components/Workspace-box.svelte
+++ src/lib/components/interface-components/sub-interface-components/Workspace-box.svelte
@@ -1,5 +1,10 @@
 <script>
-  import { activeWorkspace, allBoards, breadCrumbsItems, currentDashboardSubInterface, modalChosenColor, oldFavoriteStatus, selectedBoard, selectedWorkspace, workspaceSettingsModalActive} from "$lib/stores/global-store"
+  //@ts-nocheck
+  import { activeWorkspace, allBoards, breadCrumbsItems, currentDashboardSubInterface, modalChosenColor, oldFavoriteStatus,selectedWorkspace, workspaceSettingsModalActive, userData, activeSubject, notifs} from "$lib/stores/global-store"
+  import constants from "$lib/config/constants"
+  import bcrypt from "bcryptjs"
+  import { Icon } from 'svelte-materialify'
+  import { mdiStar, mdiStarOutline } from '@mdi/js'
 
   // export only the active workspace
   export let workspace = {
@@ -38,25 +43,98 @@
       hold += 1
     }, 150)
   }
+
+  const setFavorite = e => {
+    selectedWorkspace.set(workspace)
+    modalChosenColor.set(workspace.color)
+
+    fetch(`${constants.backURI}/MainApp/subject/workspace/edit`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ids: {
+          user: $userData.id,
+          subject: $activeSubject.id
+        },
+        workspace: {
+          id: $selectedWorkspace.id,
+          color: $modalChosenColor,
+          isFavorite: $selectedWorkspace.isFavorite ? false : true,
+          name: $selectedWorkspace.name
+        }
+      })
+    }).then(async res => {
+      const data = await res.json()
+
+      let userDataCopy = $userData
+      userDataCopy.subjects.every(subject => {
+        if(subject.id === $activeSubject.id) {
+          subject.workspaces.every(workspacea => {
+            if(workspacea.id === $selectedWorkspace.id) {
+              workspacea.isFavorite = data.workspace.isFavorite
+              selectedWorkspace.set(workspacea)
+              workspace.isFavorite = workspacea.isFavorite
+              return false
+            }
+            return true
+          })
+          activeSubject.set(subject)
+          return false
+        }
+        return true
+      })
+      userData.set(userDataCopy)
+
+      $notifs = [...$notifs, {
+        msg: `${workspace.name} is ${workspace.isFavorite ? 'set to' : 'removed from'} favorites`,
+        type: 'success',
+        id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
+      }]
+
+    }).catch(err => {
+      $notifs = [...$notifs, {
+        msg: `Error in marking as favorite: workspace, ${err}`,
+        type: 'error',
+        id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
+      }]
+      console.error(err)
+    })
+  }
+
+  let hovering = false
+  let outerWidth
 </script>
+
+<svelte:window bind:outerWidth />
 
 <div
   on:touchend={e => {
+    if(hovering) return
     if(hold < 2) {
       hold = 0
       clearInterval(timer)
     }
   }}
   on:mouseup={e => {
+    if(hovering) return
     if(hold < 2) {
       hold = 0
       clearInterval(timer)
     }
   }}
-  on:mousedown={startTimer}
-  on:touchstart={startTimer}
+  on:mousedown={e => {
+    if(hovering) return
+    startTimer()
+  }}
+  on:touchstart={e => {
+    if(hovering) return
+    startTimer()
+  }}
   on:contextmenu|preventDefault={handleRightClick}
   on:click={e => {
+    if(hovering) return
     activeWorkspace.set(workspace)
     allBoards.set(workspace.boards)
     selectedWorkspace.set(workspace)
@@ -68,22 +146,23 @@
   }}
   on:mouseenter={e => mouseEnter = true }
   on:mouseleave={e => mouseEnter = false }
-  class="has-transition notification rounded {mouseEnter?`has-background-${workspace.color}-dark`:""} is-{workspace.color}">
-  
-  <p class="parent-hover-txt-color-white quicksands has-transition has-text-weight-semibold is-unselectable txt-size-12 max-w-60p txt-overflow-ellipsis overflow-x-hidden">
+  class="has-transition notification rounded maxmins-h-110 {mouseEnter?`has-background-${workspace.color}-dark`:""} is-{workspace.color} maxmins-w-{outerWidth < 571 && outerWidth >= 473 ? '200' : '230'}"
+>
+  <!-- favorite icon -->
+  <div
+    on:mouseenter={e => hovering = true}
+    on:mouseleave={e => hovering = false}
+    on:click={setFavorite}
+    class="is-clickable pos-abs pos-r-10 pos-t-5"
+  >
+    {#if workspace.isFavorite}
+    <Icon class='has-text-{workspace.color === 'warning' ? '' : 'warning'}' path={hovering ? mdiStarOutline : mdiStar} />
+    {:else}
+    <Icon class='has-text-{workspace.color === 'warning' ? '' : 'warning'}' path={hovering ? mdiStar : mdiStarOutline} />
+    {/if}
+  </div>
+
+  <p class="parent-hover-txt-color-white inter-reg has-transition is-unselectable txt-size-14 has-text-weight-semibold pos-abs pos-r-10 pos-b-10 max-w-{outerWidth < 571 && outerWidth > 473 ? '180' : '200'} txt-overflow-ellipsis overflow-x-hidden mb-0">
     {workspace.name}
   </p>
 </div>
-
-<style>
-  .notification {
-    width: 250px;
-    height: 110px;
-  }
-  p {
-    position: absolute;
-    bottom: 10%;
-    right: 7%;
-    font-size: 100%;
-  }
-</style>
