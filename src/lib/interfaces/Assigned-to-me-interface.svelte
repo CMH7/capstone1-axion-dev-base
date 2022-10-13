ec985ad53955040c3d5a21a6ee1ef03334551f13
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-13 18:56:55 +0800

    Fixed Assigned to me interface

--- src/lib/interfaces/Assigned-to-me-interface.svelte
+++ src/lib/interfaces/Assigned-to-me-interface.svelte
@@ -1,44 +1,99 @@
 <script>
+  //@ts-nocheck
   import { fade } from 'svelte/transition'
-  import TaskCard from '$lib/components/interface-components/sub-interface-components/task/Task-card.svelte'
+  import TaskView from '$lib/components/Assigne-to-me/task-view.svelte'
   import { userData } from '$lib/stores/global-store'
+	import TaskCard from '$lib/components/interface-components/sub-interface-components/task/Task-card.svelte';
+	import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
+	import { mdiArrowUpDropCircle, mdiChevronUp } from '@mdi/js';
+  import { Icon } from 'svelte-materialify'
 
+  let outerWidth
   let assignedTasks = []
-  userData.subscribe(account => {
-    account.subjects.map(subject => {
-      subject.workspaces.map(workspace => {
-        workspace.boards.map(board => {
-          board.tasks.map(task => {
-            task.members.every(member => {
-              if(member.email === account.email) {
-                assignedTasks.push(task)
-                return false
+  let subjects = []
+  let subjectsName = []
+
+  $userData.subjects.map(subject => {
+    subject.workspaces.map(workspace => {
+      workspace.boards.map(board => {
+        board.tasks.map(task => {
+          task.members.map(member => {
+            if(member.email === $userData.email) {
+              if(!subjects.includes(subject.id)) {
+                subjects.push(subject.id)
+                subjectsName.push(subject.name)
               }
-            })
+
+              assignedTasks.push({
+                subject: {
+                  id: subject.id,
+                  name: subject.name
+                },
+                status: {
+                  id: board.id,
+                  name: board.name
+                },
+                task
+              })
+            }
           })
         })
       })
     })
   })
+
+  let phase = 1
 </script>
 
 <svelte:head>
   <title>Assigned to me</title>
 </svelte:head>
 
-<div in:fade class="hero">
-  <div class="hero-head px-3">
-    <p class="mb-0 quicksands is-size-1-tablet is-size-3-mobile has-text-weight-bold has-text-success is-unselectable">
+<svelte:window bind:outerWidth />
+
+<div in:fade class="maxmins-w-100p px-3">
+  <div class="is-flex is-justify-content-space-between is-align-items-center pt-3 py-3">
+    <div class="is-size-{outerWidth < 426 ? '6' : '5'} has-text-grey-dark fredoka-reg {phase == 2 ? 'undisp' : ''}">
       Assigned to me
-    </p>
+    </div>
   </div>
-  <div class="hero-body pt-0 mt-0">
-    <div class="columns is-multiline">
-      {#each assignedTasks as task}  
-        <div class="column">
-          <TaskCard {task} />
-        </div>
+
+  <!-- container -->
+  <div class="is-flex">
+    <!-- left side  -->
+    <div class="{outerWidth < 571 ? '': 'border-w-r-3 border-r-color-yaz-grey border-type-r-solid'} pr-3 maxmins-w-{outerWidth < 571 ? '100p' : '300 mr-3'} {phase == 2 ? 'undisp' : ''} ">
+      <ExpansionPanels flat>
+      {#each subjects as subject, i}
+        <ExpansionPanel class="has-background-grey-light hover-bg-grey-dark mb-3">
+          <span slot="header">{subjectsName[i]}</span>
+          <div class="maxmins-w-100p is-flex is-flex-direction-column is-align-items-center">
+            {#each assignedTasks as data}
+              {#if subject === data.subject.id}
+              <div on:click={e => {
+                if(outerWidth < 571) {
+                  phase = 2
+                }
+              }} >
+                <TaskCard task={data.task} boardID={data.status.id}/>
+              </div>
+              {/if}
+            {/each}
+          </div>
+        </ExpansionPanel>
       {/each}
+      </ExpansionPanels>
+    </div>
+
+    <!-- right side  -->
+    <div class="maxmins-w-{outerWidth < 571 ? '100p' : '75p'} min-h-fit-content {outerWidth < 571 && phase == 1 ? 'undisp' : ''}">
+      {#if outerWidth < 571 && phase == 2}
+      <div
+        on:click={e => phase = 1}
+        class="is-flex is-justify-content-center is-clickable pb-3">
+        <Icon class="has-text-grey" size="25px" path={mdiChevronUp} />
+      </div>
+      {/if}
+      <TaskView />
     </div>
   </div>
 </div>
