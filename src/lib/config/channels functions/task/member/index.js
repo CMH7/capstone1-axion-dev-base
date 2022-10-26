ea1f5015d8f2d3a58251364f73953a9776ad455b
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-26 17:26:20 +0800

    Added collections of all realtime functions related to task assignments members

---
+++ src/lib/config/channels functions/task/member/index.js
@@ -0,0 +1,7 @@
+import newMember from "./newMember";
+import removeMember from "./removeMember";
+
+export default {
+  newAssignee: newMember.newAssignee,
+  removeAssignment: removeMember.removeAssignment
+}
