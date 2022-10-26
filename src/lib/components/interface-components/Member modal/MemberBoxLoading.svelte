d4f55a96c9d1d3704c4379f4ea370a91dcc213eb
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-26 16:57:19 +0800

    Fixed size of the box loader

--- src/lib/components/interface-components/Member modal/MemberBoxLoading.svelte
+++ src/lib/components/interface-components/Member modal/MemberBoxLoading.svelte
@@ -1,9 +1,13 @@
 <script>
   // @ts-ignore
   import Skeleton from 'svelte-skeleton/Skeleton.svelte'
+  let outerWidth
 </script>
 
-<div class="box shadow-inside-default p-4 is-flex is-align-items-center is-justify-content-space-between">
+<svelte:window bind:outerWidth />
+
+<div class="box shadow-inside-default maxmins-h-60 mx-1 my-1 p-{outerWidth < 483 ? '2' : '4'} is-flex is-align-items-center is-justify-content-space-between has-transition hover-bg-grey-lighter maxmins-w-{outerWidth < 853 && outerWidth > 375 ? '300' : outerWidth < 376 ? '230' : '350'}">
+  <!-- profile and infos -->
   <div class="is-flex is-align-items-center">
     <div class="is-clickable">
       <Skeleton height={35} width={35} >
@@ -18,6 +22,8 @@
       </Skeleton>
     </div>
   </div>
+
+  <!-- action button -->
   <div class="is-size-7 is-clickable">
     <Skeleton width={30} height={17}>
       <rect width={30} height={17} />
