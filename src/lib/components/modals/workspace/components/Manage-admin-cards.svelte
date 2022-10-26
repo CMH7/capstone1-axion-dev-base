832032dd7cec1b0fb85582c4933794776ab7a5af
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-26 17:13:27 +0800

    Fixed bug on adding and removing an admin in the admin cards

--- src/lib/components/modals/workspace/components/Manage-admin-cards.svelte
+++ src/lib/components/modals/workspace/components/Manage-admin-cards.svelte
@@ -13,7 +13,7 @@
 
   activeWorkspace.subscribe(workspace => [
     workspace.admins.every(admin => {
-      if(admin === member.email) {
+      if(admin.email === member.email) {
         type = 'admin'
         return false
       }
@@ -24,27 +24,42 @@
   let outerWidth = 0
 
   // for local animation
-  let isAdding = false
-  let isRemoving = false
+  let localProcessing = false
 
   const memberToAdmin = () => {
+    $notifs = [...$notifs, {
+      msg: `Promoting ${member.name} as an admin. Please wait.`,
+      type: 'wait',
+      id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
+    }]
     isProcessing.set(true)
-    isAdding = true
+    localProcessing = true
 
-    fetch(`${constants.backURI}/MainApp/dashboard/subject/workspace/create/admin`, {
+    fetch(`${constants.backURI}/MainApp/dashboard/subject/workspace/admin/add`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         ids: {
-          user: $userData.id,
+          userA: $userData.id,
+          userB: member.id,
           subject: $activeSubject.id,
           workspace: $activeWorkspace.id
         },
-        email: member.email
+        admin: {
+          email: member.email,
+          name: member.name,
+          id: member.id
+        }
       })
     }).then(async res => {
+      $notifs = [...$notifs, {
+        msg: `${member.name} is promoted as an admin!`,
+        type: 'success',
+        id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
+      }]
+
       const { admin } = await res.json()
       
       let userDataCopy = $userData
@@ -61,73 +76,52 @@
         }
         return true
       })
-      
       userData.set(userDataCopy)
-      let notifsCopy = $notifs
-      notifsCopy.push({
-        msg: `${member.name} is assigned as admin`,
-        type: 'success',
-        id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
-      })
-      notifs.set(notifsCopy)
-
       type = 'admin'
-      isAdding = false
       isProcessing.set(false)
+      localProcessing = false
     }).catch(err => {
-      let notifsCopy = $notifs
-      notifsCopy.push({
+      $notifs = [...$notifs, {
         msg: `Error in adding ${member.name} as admin. ${err}`,
         type: 'error',
         id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
-      })
-      notifs.set(notifsCopy)
-      let userDataCopy = $userData
-      userDataCopy.subjects.every(subject => {
-        if(subject.id === $activeSubject.id) {
-          subject.workspaces.every(workspace => {
-            if(workspace.id === $activeWorkspace.id) {
-              workspace.admins = workspace.admins.filter(admin => admin !== member.email)
-              return false
-            }
-            return true
-          })
-          return false
-        }
-        return true
-      })
-      type = 'member'
-      userData.set(userDataCopy)
+      }]
+      localProcessing = false
       isProcessing.set(false)
     })
   }
 
   const adminRemove = () => {
+    $notifs = [...$notifs, {
+      msg: `Demoting admin ${member.name}. Please wait.`,
+      type: 'wait',
+      id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
+    }]
     isProcessing.set(true)
-    isRemoving = true
+    localProcessing = true
 
-    fetch(`${constants.backURI}/MainApp/subject/workspace/delete/admin`, {
+    fetch(`${constants.backURI}/MainApp/subject/workspace/admin/remove`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         ids: {
-          user: $userData.id,
+          userA: $userData.id,
           subject: $activeSubject.id,
-          workspace: $activeWorkspace.id
+          workspace: $activeWorkspace.id,
+          userB: member.id
         },
-        admin: member.email
       })
     }).then(async res => {
-      const { admins } = await res.json()
+      const { adminID } = await res.json()
       
       let userDataCopy = $userData
       userDataCopy.subjects.every(subject => {
         if(subject.id === $activeSubject.id) {
           subject.workspaces.every(workspace => {
             if(workspace.id === $activeWorkspace.id) {
-              workspace.admins = admins
+              workspace.admins = workspace.admins.filter(admin => admin.id !== adminID)
               return false
             }
             return true
@@ -136,61 +130,43 @@
         }
         return true
       })
-      
       userData.set(userDataCopy)
-      let notifsCopy = $notifs
-      notifsCopy.push({
-        msg: `${member.name} is assigned as member`,
+
+      $notifs = [...$notifs, {
+        msg: `${member.name} is demoted as member`,
         type: 'success',
         id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
-      })
-      notifs.set(notifsCopy)
+      }]
 
       type = 'member'
-      isRemoving = false
+      localProcessing = false
       isProcessing.set(false)
     }).catch(err => {
-      let notifsCopy = $notifs
-      notifsCopy.push({
+      $notifs = [...$notifs, {
         msg: `Error in removing ${member.name} as admin. ${err}`,
         type: 'error',
         id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
-      })
-      notifs.set(notifsCopy)
-      let userDataCopy = $userData
-      userDataCopy.subjects.every(subject => {
-        if(subject.id === $activeSubject.id) {
-          subject.workspaces.every(workspace => {
-            if(workspace.id === $activeWorkspace.id) {
-              workspace.admins = workspace.admins.filter(admin => admin !== member.email)
-              return false
-            }
-            return true
-          })
-          return false
-        }
-        return true
-      })
+      }]
       type = 'admin'
-      userData.set(userDataCopy)
       isProcessing.set(false)
+      localProcessing = false
     })
   }
 </script>
 
 <svelte:window bind:outerWidth />
 
-<div class="parent maxmins-w-100p maxmins-h-40 bg-color-yaz-white-card rounded is-flex is-align-items-center is-justify-content-space-between p-2 mb-2 hover-bg-grey-light {isAdding ? 'has-background-grey-light': ''} has-transition">
+<div class="parent maxmins-w-100p maxmins-h-40 bg-color-yaz-white-card rounded is-flex is-align-items-center is-justify-content-space-between p-2 mb-2 hover-bg-grey-light {localProcessing ? 'has-background-grey-light': ''} has-transition">
   <div class="is-flex is-align-items-center">
     <!-- pic -->
     {#if member.profile !== ''}
-    <Avatar size={33}>
-      <img src={`${member.profile}`} alt={`${member.name}`}>
-    </Avatar>
+      <Avatar size={30} class="maxmins-w-30 maxmins-h-30">
+        <img src={`${member.profile}`} alt={`${member.name}`}>
+      </Avatar>
     {:else}
-    <Avatar size={33}>
-      <Icon class='white blue-text' path={mdiAccountCircleOutline} />
-    </Avatar>
+      <Avatar size={30} class="maxmins-w-30 maxmins-h-30">
+        <Icon class='white blue-text' path={mdiAccountCircleOutline} />
+      </Avatar>
     {/if}
   
     <!-- name -->
@@ -199,41 +175,30 @@
     </div>
   </div>
 
-  <!-- minus or plus -->
   <div class="is-flex is-align-items-center">
-    <span class="txt-size-{outerWidth < 376 ? '10': '14'} mr-2 has-text-{type === 'admin'? 'success': type === 'admin' ? 'warning' : 'primary'}-dark">{type === 'member'? 'member' : member.email === $userData.email ? 'owner/admin' : type}</span>
+    <!-- labeling -->
+    <span class="txt-size-{outerWidth < 376 ? '10': '14'} mr-2 has-text-{type === 'admin'? 'success': type === 'admin' ? 'warning' : 'primary'}-dark">{type === 'member'? 'member' : $activeWorkspace.createdBy === member.name ? 'owner/admin' : type}</span>
+
+    <!-- minus or plus -->
     <div class="is-flex is-align-items-center is-justify-content-center mr-1 is-clickable">
-      {#if type === 'admin' && member.email !== $userData.email}
-      <div
-        on:click={e => {
-          if(!$isProcessing) {
-            adminRemove()
-          }
-        }}
-      >
-        {#if !isRemoving}
-        <Icon class='hover-txt-color-danger' size={20} path={mdiMinus} />
-        {:else}
-        <Pulse color='#f56b6b' size={20} />
-        {/if}
-      </div>
-      {:else if member.email === $userData.email}
-      <div class="maxmins-w-20 maxmins-h-20p"></div>
+      {#if type === 'admin' && (member.email === $userData.email || member.name === $activeWorkspace.createdBy)}
+        <div class="maxmins-w-20 maxmins-h-20p"></div>
+      {:else if type === 'admin'}
+        <div on:click={e => {if(!$isProcessing) adminRemove()}}>
+          {#if !localProcessing}
+            <Icon class='hover-txt-color-danger' size={20} path={mdiMinus} />
+          {:else}
+            <Pulse color='#f56b6b' size={20} />
+          {/if}
+        </div>
       {:else}
-      <div
-        on:click={e => {
-          if(!$isProcessing) {
-            memberToAdmin()
-          }
-        }}
-        class="is-flex is-justify-content-center is-align-items-center"
-      >
-        {#if !isAdding}
-        <Icon class='hover-txt-color-warning' size={20} path={mdiPlus} />
-        {:else}
-        <Pulse color='#fddd3f' size={20} />
-        {/if}
-      </div>
+        <div on:click={e => {if(!$isProcessing) memberToAdmin()}} class="is-flex is-justify-content-center is-align-items-center">
+          {#if !localProcessing}
+            <Icon class='hover-txt-color-warning' size={20} path={mdiPlus} />
+          {:else}
+            <Pulse color='#fddd3f' size={20} />
+          {/if}
+        </div>
       {/if}
     </div>
   </div>
