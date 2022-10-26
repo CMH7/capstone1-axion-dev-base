3b6235e459c8fd31c7a373a2bdaaf21d439a7f2a
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-26 17:01:08 +0800

    Code revision

--- src/lib/components/modals/deletions/Subject-deletions-modal.svelte
+++ src/lib/components/modals/deletions/Subject-deletions-modal.svelte
@@ -1,16 +1,65 @@
 <script>
   //@ts-nocheck
   import { Dialog, Button } from 'svelte-materialify'
-  import { subjectDeletionModalActive, selectedSubjectForSubjectSettings, subjectSettingsModalActive, userData, notifs, currentInterface,  } from '$lib/stores/global-store'
+  import { subjectDeletionModalActive, selectedSubjectForSubjectSettings, subjectSettingsModalActive, userData, notifs, currentInterface, currentDashboardSubInterface, breadCrumbsItems, isProcessing, activeSubject,  } from '$lib/stores/global-store'
   import bcrypt from 'bcryptjs'
   import constants from '$lib/config/constants'
+  import { Pulse } from 'svelte-loading-spinners'
+
+  const deleteSubject = e => {
+    if($isProcessing) return
+    isProcessing.set(true)
+
+    // do http requests here
+    fetch(`${constants.backURI}/MainApp/dashboard/subject/delete`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ids: {
+          user: $userData.id,
+          subject: $selectedSubjectForSubjectSettings.id
+        }
+      })
+    }).then(async res => {
+      const { subjectID } = await res.json()
+      let userDataCopy = $userData
+      userDataCopy.subjects = userDataCopy.subjects.filter(subject => subject.id !== subjectID)
+      userData.set(userDataCopy)
+
+      $notifs = [...$notifs, {
+        msg: `${$selectedSubjectForSubjectSettings.name} has been deleted`,
+        type: 'success',
+        id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
+      }]
+
+      if($currentDashboardSubInterface === 'Workspaces') {
+        $breadCrumbsItems = [{text: 'Subjects'}]
+        currentDashboardSubInterface.set('Subjects')
+      }
+
+      subjectDeletionModalActive.set(false)
+      selectedSubjectForSubjectSettings.set(constants.subject)
+      activeSubject.set(constants.subject)
+      isProcessing.set(false)
+        
+    }).catch(err => {
+      $notifs = [...$notifs, {
+        msg: `Error in deleting subject, ${err}`,
+        type: 'error',
+        id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
+      }]
+      isProcessing.set(false)
+    })
+  }
 </script>
 
 <div>
   <Dialog persistent bind:active={$subjectDeletionModalActive} class='has-background-white p-2' >
     <div class="maxmins-w-100p">
       <div class="txt-size-13 mb-3 inter-reg">
-        <span class="has-text-weight-bold">This action cannot be undone.</span> Doing this will delete all workspaces, boards, tasks and files under this subject. Click confirm button to finally delete the subject. <span class="tag is-danger inter-reg">delete {`${$selectedSubjectForSubjectSettings.name}`}</span>
+        <span class="has-text-weight-bold">This action cannot be undone.</span> Doing this will delete all workspaces and its copy to other members, boards, tasks and files under this subject. Click confirm button to finally delete the subject. <span class="tag is-danger inter-reg">delete {`${$selectedSubjectForSubjectSettings.name}`}</span>
       </div>
       <div class="maxmins-w-100p is-flex is-flex-direction-row-reverse is-justify-content-flex-start">
         <div
@@ -18,72 +67,16 @@
             subjectDeletionModalActive.set(false)
             subjectSettingsModalActive.set(true)
           }}
+          class='{$isProcessing ? 'undisp': ''}'
         >
           <Button depressed size='small'>Cancel</Button>
         </div>
-        <div
-          on:click={e => {
-            let userDataCopy = $userData
-            userDataCopy.subjects = userDataCopy.subjects.filter(subject => subject.id != $selectedSubjectForSubjectSettings.id)
-            userData.set(userDataCopy)
-
-            //local addition of notification (user)
-            const localNotification = {
-              id: bcrypt.hashSync(`${$selectedSubjectForSubjectSettings.name} subject has been deleted.`, Math.ceil(Math.random() * 10)),
-              message: `${$selectedSubjectForSubjectSettings.name} subject has been deleted.`,
-              isRead: false,
-              anInvitation: false,
-              aMention: false,
-              conversationID: '',
-              fromInterface: {
-                interf: `${$currentInterface}`,
-                subInterface: `Subjects`
-              },
-              fromTask: '',
-              for: {
-                self: true,
-                userID: `${$userData.id}`
-              }
-            }
-            userDataCopy.notifications.push(localNotification)
-            
-            userData.set(userDataCopy)
-            subjectDeletionModalActive.set(false)
-
-            // do http requests here
-            fetch(`${constants.backURI}/MainApp/delete/subject`, {
-              method: 'DELETE',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                ids: {
-                  user: $userData.id,
-                  subject: $selectedSubjectForSubjectSettings.id
-                },
-                notification: localNotification
-              })
-            }).then(async res => {
-              const { error } = await res.json()
-              let notifsCopy = $notifs
-               notifsCopy.push({
-                 msg: `${$selectedSubjectForSubjectSettings.name} has been deleted`,
-                 type: 'success',
-                 id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
-               })
-               notifs.set(notifsCopy)
-            }).catch(err => {
-             let notifsCopy = $notifs
-              notifsCopy.push({
-                msg: `Error in deleting subject, ${err}`,
-                type: 'error',
-                id: bcrypt.hashSync(`${new Date().getMilliseconds() * (Math.random() * 1)}`, 13)
-              })
-              notifs.set(notifsCopy)
-            })
-          }}
-        >
+        <div on:click={deleteSubject}>
+           {#if !$isProcessing}
           <Button depressed size='small'>Confirm</Button>
+          {:else}
+          <Pulse size={20} color='#191a48' />
+          {/if}
         </div>
       </div>
     </div>
