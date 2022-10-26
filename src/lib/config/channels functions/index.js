72196183a7f5cad95d88d56a1fd575d97033ace6
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-26 17:16:57 +0800

    Added collections of all functions for the realtime operations

---
+++ src/lib/config/channels functions/index.js
@@ -0,0 +1,36 @@
+import account from './account'
+import invitation from './invitation'
+import subject from './subject'
+import task from './task'
+import workspace from './workspace'
+import board from './workspace/board'
+
+export default {
+  // #### TASKS ####
+  taskRenamed: task.taskRenamed,
+  newChat: task.newChat,
+  taskViewer: task.taskViewer,
+  taskNew: task.taskNew,
+  taskDescription: task.taskDescription,
+  taskStatusChanged: task.taskStatusChanged,
+  taskNewAssignee: task.taskNewAssignee,
+  taskRemoveAssignment: task.taskRemoveAssignment,
+  // #### WORKSPACE ####
+  onDelete: workspace.onDelete,
+  onAdminRemoved: workspace.adminRemoved,
+  onNewAdmin: workspace.onNewAdmin,
+  onMemberRemoved: workspace.onMemberRemoved,
+  onMemberLeave: workspace.onMemberLeave,
+  // #### SUBJECT ####
+  onSubjectDeletedByOwner: subject.subjectDeletedByOwner,
+  // #### ACCOUNT ####
+  accountEmailVerified: account.accountEmailVerified,
+  // #### INVITATION ####
+  invitationRejected: invitation.rejected,
+  invitationAccepted: invitation.accepted,
+  ownerInvitationAccepted: invitation.accepted2,
+  ownerInvitationCanceled: invitation.canceled,
+  newInvitation: invitation.new,
+  // #### BOARD ####
+  newBoard: board.newBoard
+}
