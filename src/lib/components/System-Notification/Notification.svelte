b1319f01677ab76992dec2a2169f001c9fbac1d3
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-10-30 22:53:38 +0800

    Nothing

--- src/lib/components/System-Notification/Notification.svelte
+++ src/lib/components/System-Notification/Notification.svelte
@@ -24,6 +24,8 @@
   <div on:click={remove} class="delete" />
   <div class="is-flex is-flex-direction-column">
     {notif.msg} {#if notif.type === 'stayError'} {' Help the developers solve it by reporting it to the following link:'} {/if}
+    
+    <!-- stayError -->
     {#if notif.type === 'stayError'}
     <span class="button is-success mt-3">
       <a href="https://height.app/sWtP0Ak7Nr/?taskForm=Axion-Bug-and-Recommendation-form-vAjPyLmwfwBl" target="_blank" rel="noopener noreferrer">Axion developers support</a>
