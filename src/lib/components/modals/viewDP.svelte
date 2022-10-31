56e7231f6f4a9b1cecca4db05692beaa45481c3b
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-11-01 03:13:58 +0800

    Added modal for viewing user profile picture

---
+++ src/lib/components/modals/viewDP.svelte
@@ -0,0 +1,10 @@
+<script>
+  //@ts-nocheck
+	import { userData } from '$lib/stores/global-store';
+  import { Dialog } from 'svelte-materialify'
+  import { viewDPModalActive, currentDP } from '$lib/stores/myProfile'
+</script>
+
+<Dialog bind:active={$viewDPModalActive} class='p-0 m-0 is-flex is-justify-content-center is-shadowless is-clickable'>
+  <img on:click={e => viewDPModalActive.set(false)} src="{$currentDP}" alt="{$userData.firstName} {$userData.lastName} profile picture">
+</Dialog>
