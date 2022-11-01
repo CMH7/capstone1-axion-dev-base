a50ff763fc4eff6783589fcc870101bed43c07e3
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-11-01 15:20:24 +0800

    Fixed unwanted empty task view

--- src/lib/interfaces/Assigned-to-me-interface.svelte
+++ src/lib/interfaces/Assigned-to-me-interface.svelte
@@ -12,6 +12,7 @@
   let assignedTasks = []
   let subjects = []
   let subjectsName = []
+  let taskViewNow = false
 
   $userData.subjects.map(subject => {
     subject.workspaces.map(workspace => {
@@ -62,7 +63,7 @@
   <div class="is-flex">
     <!-- left side  -->
     <div class="{outerWidth < 571 ? '': 'border-w-r-3 border-r-color-yaz-grey border-type-r-solid'} pr-3 maxmins-w-{outerWidth < 571 ? '100p' : '300 mr-3'} {phase == 2 ? 'undisp' : ''} ">
-      <ExpansionPanels flat>
+      <ExpansionPanels popout flat>
       {#each subjects as subject, i}
         <ExpansionPanel class="has-background-grey-light hover-bg-grey-dark mb-3">
           <span slot="header">{subjectsName[i]}</span>
@@ -72,6 +73,8 @@
               <div on:click={e => {
                 if(outerWidth < 571) {
                   phase = 2
+                }else{
+                  taskViewNow = true
                 }
               }} >
                 <TaskCard task={data.task} boardID={data.status.id}/>
@@ -87,13 +90,15 @@
     <!-- right side  -->
     <div class="maxmins-w-{outerWidth < 571 ? '100p' : '75p'} min-h-fit-content {outerWidth < 571 && phase == 1 ? 'undisp' : ''}">
       {#if outerWidth < 571 && phase == 2}
-      <div
-        on:click={e => phase = 1}
-        class="is-flex is-justify-content-center is-clickable pb-3">
-        <Icon class="has-text-grey" size="25px" path={mdiChevronUp} />
-      </div>
+        <div
+          on:click={e => phase = 1}
+          class="is-flex is-justify-content-center is-clickable pb-3">
+          <Icon class="has-text-grey" size="25px" path={mdiChevronUp} />
+        </div>
       {/if}
-      <TaskView />
+      {#if taskViewNow}
+        <TaskView />
+      {/if}
     </div>
   </div>
 </div>
